e423071a408edeef2c4d9b380dbdc7085fad8c2e
Megha Sri Satya Sai Devineni <meghasri.devineni@gmail.com>
2023-11-08 15:40:04 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import Clock from "./components/Clock";
+import Day from "./components/Day";
+import Weather from "./components/Weather";
+
+function App() {
+  return (
+    <div className="position-on-mirror"> 
+
+    <div className="App">
+      <header className="App-header">
+        <p>Hello there</p>
+        <div> 
+          <Day />
+          <Clock />
+        </div>
+        <div>
+          <Weather /> 
+        </div>
+      </header>
+    </div>
+    </div>
+  );
+}
+
+export default App;
